0a3c5b49211e2af0d7d12369cfc49f0ac7a50afe
Sahil <sahilreddy99@gmail.com>
2023-08-06 17:19:53 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,92 @@
+
+
+
+for(let i=0;i<document.querySelectorAll(".drum").length;i++){
+
+document.querySelectorAll(".drum")[i].addEventListener("click", handleClick );//here we running for loop to add event listeners to all the buttons here
+
+function handleClick(){
+
+  // let audio=new Audio("sounds/tom-1.mp3");// we created a new object named audio over here
+  // audio.play();
+  // console.log(this.innerHTML);
+  // this.style.color = "white";//to make the color of the button to be white after clicking on it
+  let button=this.innerHTML
+  switch(button){
+
+case "w"://this should be inside ""
+  let tom_1=new Audio("sounds/tom-1.mp3")//url should be inside " "
+  tom_1.play()
+  break;
+case "a":
+let tom_2=new Audio("sounds/tom-2.mp3")
+tom_2.play()
+    break;
+case "s":
+let tom_3=new Audio("sounds/tom-3.mp3")
+tom_3.play()
+      break;
+case "d":
+let tom_4=new Audio("sounds/tom-4.mp3")
+tom_4.play()
+        break;
+case "j":
+let snare=new Audio("sounds/snare.mp3")
+snare.play()
+  break;
+case "k":
+let crash=new Audio("sounds/crash.mp3")
+crash.play()
+  break;
+case "l":
+let kick=new Audio("sounds/kick-bass.mp3")
+kick.play()
+  break;
+default:
+    console.log("not valid");
+
+
+  }
+
+}}
+
+
+document.addEventListener("keydown",function(event){
+
+let key=event.key
+switch(key){
+
+case "w"://this should be inside ""
+let tom_1=new Audio("sounds/tom-1.mp3")//url should be inside " "
+tom_1.play()
+break;
+case "a":
+let tom_2=new Audio("sounds/tom-2.mp3")
+tom_2.play()
+  break;
+case "s":
+let tom_3=new Audio("sounds/tom-3.mp3")
+tom_3.play()
+    break;
+case "d":
+let tom_4=new Audio("sounds/tom-4.mp3")
+tom_4.play()
+      break;
+case "j":
+let snare=new Audio("sounds/snare.mp3")
+snare.play()
+break;
+case "k":
+let crash=new Audio("sounds/crash.mp3")
+crash.play()
+break;
+case "l":
+let kick=new Audio("sounds/kick-bass.mp3")
+kick.play()
+break;
+default:
+  console.log("not valid");
+
+
+}
+})
